00b8741f13bdd1da44065ae7c759906a604ab808
elpepe11 <95416897+elpepe11@users.noreply.github.com>
2022-01-19 13:49:01 +0100

    Add files via upload

---
+++ actividad3/function.c
@@ -0,0 +1,16 @@
+/*
+ * function.c
+ *
+ *  Created on: 19 ene 2022
+ *      Author: sharon
+ */
+int binario(){
+	int num, i=1;
+	printf("Introdueix un nombre");
+	scanf("%d",&num);
+
+	do{
+		printf("Error");
+	}while(num == 0)
+}
+
---
+++ actividad3/main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: 12 ene 2022
+ *      Author: sharon
+ */
+
+#include <stdio.h>
+#include "functions.h"
+
+void main(){
+	int num ,bin=0,rem=0,count=1;
+	num=binario(num,count);
+
+	rem=num % 2;
+	num=num / 2;
+	bin=bin +(rem*count);
+	i=i 10;
+
+	printf("El resultado es %d\n",&num);
+
+
+}
