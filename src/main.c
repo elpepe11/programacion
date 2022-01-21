d461e82171a9926ee1e8af6e80558399542960da
elpepe11 <95416897+elpepe11@users.noreply.github.com>
2022-01-21 12:23:20 +0100

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,21 @@
+/*
+ * main.c
+ *
+ *  Created on: 21 ene 2022
+ *      Author: sharon
+ */
+
+#include <stdio.h>
+void main(){
+	int num1=0,num2=0;
+
+	printf("Introduce Primer numero");
+	scanf("%d", &num1);
+	printf("Introduce segundo numero");
+	scanf("%d", &num2);
+
+	cambio(num1,num2);
+
+
+
+}
